e78edc26f3ee29af6ffd7a18eaa011a1c40256b9
Chris Mastel <chris.mastel@gmail.com>
2019-12-09 16:53:39 +0000

    initial commit

---
+++ main.rb
@@ -0,0 +1,11 @@
+# Classes: player, question, game
+
+# Player: will store player id and lives remaining, when they
+#         get a question wrong, the number of lives remaining will
+#          decrease by 1
+# Game: will contain the id's of the two players, track who's turn
+#       is next, and interface with the player and question classes
+# Question: will create a math question, and determine whether the 
+#           provided answer is correct
+
+
